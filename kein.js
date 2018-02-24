0ef86fb2a99e698041a9e1dc37ff34201f17bd5d
Chinozuku <lord.michael.forever@gmail.com>
2018-02-24 16:38:54 +0700

    Adding grouping 1
    testing

--- kein.js
+++ kein.js
@@ -38,6 +38,21 @@ $(document).ready(function(){
 	$('#dtKein').DataTable({
 		"data": dataSet,
 		"ordering": false,
-		"searching": false
+		"searching": false,
+		"drawCallback": function ( settings ) {
+            var api = this.api();
+            var rows = api.rows( {page:'current'} ).nodes();
+            var last=null;
+ 
+            api.column(2, {page:'current'} ).data().each( function ( group, i ) {
+                if ( last !== group ) {
+                    $(rows).eq( i ).before(
+                        '<tr class="group"><td colspan="5">'+group+'</td></tr>'
+                    );
+ 
+                    last = group;
+                }
+            } );
+        }
 	});
 });
